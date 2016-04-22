5a751cd52532904f3a916c61bff6abc37ce1bdbc
mdnurul <mailme.nurul@gmail.com>
2016-04-22 15:44:56 +0530

    nodejs http stub test initial commit

---
+++ api.js
@@ -0,0 +1,46 @@
+/**
+ * Created by bitcanny on 4/5/16.
+ */
+
+var http = require('http');
+
+module.exports = {
+    get: function(callback) {
+        var req = http.request({
+            hostname: 'jsonplaceholder.typicode.com',
+            path: '/posts/1'
+        }, function(response) {
+            var data = '';
+            response.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            response.on('end', function() {
+                callback(null, JSON.parse(data));
+            });
+        });
+
+        req.end();
+    },
+
+    post: function(data, callback) {
+        var req = http.request({
+            hostname: 'jsonplaceholder.typicode.com',
+            path: '/posts',
+            method: 'POST'
+        }, function(response) {
+            var data = '';
+            response.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            response.on('end', function() {
+                callback(null, JSON.parse(data));
+            });
+        });
+
+        req.write(JSON.stringify(data));
+
+        req.end();
+    }
+};
---
+++ test/testapi.js
@@ -0,0 +1,48 @@
+/**
+ * Created by bitcanny on 4/5/16.
+ */
+
+var assert = require('assert');
+var sinon = require('sinon');
+
+var PassThrough = require('stream').PassThrough;
+
+var http = require('http');
+
+var api = require('../api.js');
+
+describe('api', function() {
+
+    beforeEach(function() {
+        this.request = sinon.stub(http, 'request');
+    });
+
+    afterEach(function() {
+        http.request.restore();
+    });
+
+    it('should convert get result to object', function(done) {
+        var expected = { hello: 'world' };
+
+        var response = new PassThrough();
+
+        response.write(JSON.stringify(expected));
+
+        response.end();
+
+        var request = new PassThrough();
+
+        this.request.callsArgWith(1, response).returns(request);
+
+        api.get(function(err, result) {
+
+            assert.deepEqual(result, expected);
+
+            done();
+        });
+    });
+
+
+    //We will place our tests cases here
+
+});
